6add6e95435fc36a547d7314a649ba079384e992
Rohit kumar <kumar9354223529@gmail.com>
2026-06-10 12:12:34 +0530

    Day-10 uploaded

---
+++ day-10/que38.c
@@ -0,0 +1,21 @@
+/*Write a program to Print reverse pyramid.
+*********
+*******
+ *****
+ ***
+ * */
+
+ #include<stdint.h>
+ int main() {
+    int i, j;
+    for(i=9; i>=1; i--){
+        for(j=1; j<=5-i; j++ ){
+            printf(" ");
+        }
+        for(j=1; j<=(2*i-1); j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+ }
