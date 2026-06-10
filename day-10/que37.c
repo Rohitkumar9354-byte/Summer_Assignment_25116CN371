241a01a1f2a50e4906b6a824c898b9bec581cd7b
Rohit kumar <kumar9354223529@gmail.com>
2026-06-10 11:50:41 +0530

    Day-10 uploaded

--- day-10/que37.c
+++ day-10/que37.c
@@ -1,21 +1,23 @@
-/*Write a program to Print star pyramid.
- *
- ***
- *****
+/*Write a program to Print star pyramid. 
+* 
+*** 
+*****
 *******
 ********* */
 
-#include<stdio.h>
+#include <stdio.h>
+
 int main() {
     int i, j;
+
     for(i=1; i<=5; i++) {
-        for(j=1; j<=5-i; j++){
+        for(j=1; j <= 5 - i; j++) {
             printf(" ");
         }
-for(j=1; j<=(2*i-1); j++) {
-    printf("*");
-}
-printf("\n");
+        for(j=1; j<=(2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
     }
     return 0;
 }
